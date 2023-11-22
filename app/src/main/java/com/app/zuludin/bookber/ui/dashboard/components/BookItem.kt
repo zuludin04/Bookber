2bbdd12b896c00295ab25945216076ad2db096df
zuludin <zmauludin04@gmail.com>
2023-11-22 15:24:18 +0700

    fixing book item ui

--- app/src/main/java/com/app/zuludin/bookber/ui/dashboard/components/BookItem.kt
+++ app/src/main/java/com/app/zuludin/bookber/ui/dashboard/components/BookItem.kt
@@ -4,15 +4,32 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.zuludin.bookber.R
 import com.app.zuludin.bookber.data.local.entity.BookEntity
 
 @Composable
@@ -24,23 +41,67 @@ fun BookItem(
     val byteArray = Base64.decode(book.cover, Base64.DEFAULT)
     val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 
-    Column(
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 3.dp),
         modifier = modifier
             .clickable { onClick(book) }
-            .padding(8.dp),
+            .padding(10.dp)
     ) {
-        Image(
-            bitmap = bitmap.asImageBitmap(),
-            contentDescription = null,
-            modifier = Modifier.size(200.dp)
-        )
-        Text(text = book.title)
-        Text(text = book.genre)
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+        ) {
+            Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.clip(RoundedCornerShape(5.dp))
+            )
+            Text(
+                text = book.title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = book.genre,
+                fontSize = 14.sp,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.Gray
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Biography", color = Color.Gray, fontSize = 13.sp, maxLines = 1)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "10", color = Color.Gray, fontSize = 13.sp, maxLines = 1)
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_quotes),
+                        contentDescription = "",
+                        colorFilter = ColorFilter.tint(Color.DarkGray, BlendMode.SrcIn),
+                        modifier = Modifier.size(18.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun BookItemPreview() {
-    BookItem(book = BookEntity(title = "Hall", genre = "Oalah"), {})
+    BookItem(
+        book = BookEntity(
+            title = "Hall",
+            genre = "Oalah",
+            cover = "iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=="
+        ), {})
 }
