a05594d7570c74dfd18327bc37fbf273d98ab30f
zuludin <zmauludin04@gmail.com>
2023-11-24 10:07:50 +0700

    add new method for local source

--- app/src/main/java/com/app/zuludin/bookber/data/local/BookberLocalDataSource.kt
+++ app/src/main/java/com/app/zuludin/bookber/data/local/BookberLocalDataSource.kt
@@ -3,9 +3,13 @@ package com.app.zuludin.bookber.data.local
 import androidx.lifecycle.LiveData
 import com.app.zuludin.bookber.data.Result
 import com.app.zuludin.bookber.data.local.entity.BookEntity
+import com.app.zuludin.bookber.data.local.entity.CategoryEntity
+import com.app.zuludin.bookber.data.local.entity.QuoteEntity
 
 interface BookberLocalDataSource {
     fun loadBookStore(): LiveData<Result<List<BookEntity>>>
+
+    fun loadBooksByCategory(categoryId: String): LiveData<Result<List<BookEntity>>>
 
     fun loadBookDetail(bookId: String): LiveData<Result<BookEntity>>
 
@@ -14,4 +18,24 @@ interface BookberLocalDataSource {
     suspend fun updateBook(book: BookEntity)
 
     suspend fun deleteBookById(bookId: String)
+
+    fun loadAllQuotes(): LiveData<Result<List<QuoteEntity>>>
+
+    fun loadQuotesByBook(bookId: String): LiveData<Result<List<QuoteEntity>>>
+
+    fun loadQuotesByCategory(categoryId: String): LiveData<Result<List<QuoteEntity>>>
+
+    suspend fun saveQuote(quote: QuoteEntity)
+
+    suspend fun updateQuote(quote: QuoteEntity)
+
+    suspend fun deleteQuoteById(quoteId: String)
+
+    fun loadCategoriesByType(type: Int): LiveData<Result<List<CategoryEntity>>>
+
+    suspend fun saveCategory(category: CategoryEntity)
+
+    suspend fun updateCategory(category: CategoryEntity)
+
+    suspend fun deleteCategoryById(categoryId: String)
 }
--- app/src/main/java/com/app/zuludin/bookber/data/local/BookberLocalDataSourceImpl.kt
+++ app/src/main/java/com/app/zuludin/bookber/data/local/BookberLocalDataSourceImpl.kt
@@ -5,16 +5,29 @@ import androidx.lifecycle.map
 import com.app.zuludin.bookber.data.Result
 import com.app.zuludin.bookber.data.Result.Success
 import com.app.zuludin.bookber.data.local.entity.BookEntity
+import com.app.zuludin.bookber.data.local.entity.CategoryEntity
+import com.app.zuludin.bookber.data.local.entity.QuoteEntity
 import com.app.zuludin.bookber.data.local.room.BookDao
+import com.app.zuludin.bookber.data.local.room.CategoryDao
+import com.app.zuludin.bookber.data.local.room.QuoteDao
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class BookberLocalDataSourceImpl internal constructor(
-    private val bookDao: BookDao, private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val bookDao: BookDao,
+    private val quoteDao: QuoteDao,
+    private val categoryDao: CategoryDao,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : BookberLocalDataSource {
     override fun loadBookStore(): LiveData<Result<List<BookEntity>>> {
         return bookDao.loadBookStore().map {
+            Success(it)
+        }
+    }
+
+    override fun loadBooksByCategory(categoryId: String): LiveData<Result<List<BookEntity>>> {
+        return bookDao.loadBooksByCategory(categoryId).map {
             Success(it)
         }
     }
@@ -35,5 +48,54 @@ class BookberLocalDataSourceImpl internal constructor(
 
     override suspend fun deleteBookById(bookId: String) = withContext<Unit>(ioDispatcher) {
         bookDao.deleteBookById(bookId)
+    }
+
+    override fun loadAllQuotes(): LiveData<Result<List<QuoteEntity>>> {
+        return quoteDao.loadAllQuotes().map {
+            Success(it)
+        }
+    }
+
+    override fun loadQuotesByBook(bookId: String): LiveData<Result<List<QuoteEntity>>> {
+        return quoteDao.loadQuotesByBook(bookId).map {
+            Success(it)
+        }
+    }
+
+    override fun loadQuotesByCategory(categoryId: String): LiveData<Result<List<QuoteEntity>>> {
+        return quoteDao.loadQuotesByCategory(categoryId).map {
+            Success(it)
+        }
+    }
+
+    override suspend fun saveQuote(quote: QuoteEntity) = withContext(ioDispatcher) {
+        quoteDao.saveQuote(quote)
+    }
+
+    override suspend fun updateQuote(quote: QuoteEntity) = withContext<Unit>(ioDispatcher) {
+        quoteDao.updateQuote(quote)
+    }
+
+    override suspend fun deleteQuoteById(quoteId: String) = withContext<Unit>(ioDispatcher) {
+        quoteDao.deleteQuoteById(quoteId)
+    }
+
+    override fun loadCategoriesByType(type: Int): LiveData<Result<List<CategoryEntity>>> {
+        return categoryDao.loadAllCategories(type).map {
+            Success(it)
+        }
+    }
+
+    override suspend fun saveCategory(category: CategoryEntity) = withContext(ioDispatcher) {
+        categoryDao.saveCategory(category)
+    }
+
+    override suspend fun updateCategory(category: CategoryEntity) =
+        withContext<Unit>(ioDispatcher) {
+            categoryDao.updateCategory(category)
+        }
+
+    override suspend fun deleteCategoryById(categoryId: String) = withContext<Unit>(ioDispatcher) {
+        categoryDao.deleteCategoryById(categoryId)
     }
 }
