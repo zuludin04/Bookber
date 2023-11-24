530b5227c972aa91e2c1b1e866abddd05303ef8f
zuludin <zmauludin04@gmail.com>
2023-11-24 09:41:34 +0700

    add new entity to db

--- app/src/main/java/com/app/zuludin/bookber/data/local/room/BookberDatabase.kt
+++ app/src/main/java/com/app/zuludin/bookber/data/local/room/BookberDatabase.kt
@@ -3,8 +3,14 @@ package com.app.zuludin.bookber.data.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.app.zuludin.bookber.data.local.entity.BookEntity
+import com.app.zuludin.bookber.data.local.entity.CategoryEntity
+import com.app.zuludin.bookber.data.local.entity.QuoteEntity
 
-@Database(entities = [BookEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [BookEntity::class, CategoryEntity::class, QuoteEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class BookberDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
 
