c709e3670f5dfe3bfd2ab71ced9e908dec3284e2
zuludin <zmauludin04@gmail.com>
2023-11-28 15:31:43 +0700

    init book ViewModel

--- app/src/main/java/com/app/zuludin/bookber/util/ViewModelFactory.kt
+++ app/src/main/java/com/app/zuludin/bookber/util/ViewModelFactory.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.app.zuludin.bookber.domain.BookberRepository
+import com.app.zuludin.bookber.ui.book.BookViewModel
 import com.app.zuludin.bookber.ui.create.BookCreateViewModel
 import com.app.zuludin.bookber.ui.dashboard.DashboardViewModel
 import com.app.zuludin.bookber.ui.detail.BookDetailViewModel
@@ -22,6 +23,8 @@ class ViewModelFactory constructor(
             return BookDetailViewModel(bookberRepository) as T
         } else if (modelClass.isAssignableFrom(QuoteViewModel::class.java)) {
             return QuoteViewModel(bookberRepository) as T
+        } else if (modelClass.isAssignableFrom(BookViewModel::class.java)) {
+            return BookViewModel(bookberRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
     }
