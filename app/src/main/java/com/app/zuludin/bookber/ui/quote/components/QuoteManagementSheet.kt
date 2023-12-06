ed0831e7d7bdab46013fdeeb12e072f4e03f603f
zuludin <zmauludin04@gmail.com>
2023-12-06 10:06:00 +0700

    share quote text

--- app/src/main/java/com/app/zuludin/bookber/ui/quote/components/QuoteManagementSheet.kt
+++ app/src/main/java/com/app/zuludin/bookber/ui/quote/components/QuoteManagementSheet.kt
@@ -1,5 +1,6 @@
 package com.app.zuludin.bookber.ui.quote.components
 
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -31,6 +33,8 @@ fun QuoteManagementSheet(
     onDeleteQuote: (quoteId: String) -> Unit,
     onRemoveFromBook: (quote: QuoteEntity) -> Unit,
 ) {
+    val context = LocalContext.current
+
     ModalBottomSheet(onDismissRequest = { onDismissRequest() }) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row(verticalAlignment = Alignment.CenterVertically) {
@@ -52,7 +56,24 @@ fun QuoteManagementSheet(
             }
             Divider(modifier = Modifier.padding(vertical = 16.dp))
             QuoteManagementMenu(iconId = R.drawable.ic_edit, title = "Edit", onClickMenu = {})
-            QuoteManagementMenu(iconId = R.drawable.ic_share, title = "Share", onClickMenu = {})
+            QuoteManagementMenu(
+                iconId = R.drawable.ic_share,
+                title = "Share",
+                onClickMenu = {
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.type = "text/plain"
+                    val quoteFormat = """
+                        ${quote.quotes}
+                        
+                        - ${quote.author}
+                    """.trimIndent()
+                    intent.putExtra(Intent.EXTRA_TEXT, quoteFormat)
+                    context.startActivity(
+                        Intent.createChooser(intent, "Share Quote from ${quote.author}")
+                    )
+                    onDismissRequest()
+                }
+            )
             if (quote.bookId != "") {
                 QuoteManagementMenu(
                     iconId = R.drawable.ic_remove,
