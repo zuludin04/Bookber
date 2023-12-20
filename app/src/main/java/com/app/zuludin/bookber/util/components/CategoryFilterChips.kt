fc0e8a3f0827fd3cc5c35671659ff3dfdae6e03a
zuludin <zmauludin04@gmail.com>
2023-12-20 14:30:40 +0700

    selected category ui state

--- app/src/main/java/com/app/zuludin/bookber/util/components/CategoryFilterChips.kt
+++ app/src/main/java/com/app/zuludin/bookber/util/components/CategoryFilterChips.kt
@@ -1,6 +1,8 @@
 package com.app.zuludin.bookber.util.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,16 +12,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.zuludin.bookber.data.local.entity.CategoryEntity
@@ -27,6 +27,8 @@ import com.app.zuludin.bookber.util.bottomBorder
 
 @Composable
 fun CategoryFilterChips(categories: List<CategoryEntity>) {
+    var selectedChip by remember { mutableStateOf("All") }
+
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
@@ -34,23 +36,35 @@ fun CategoryFilterChips(categories: List<CategoryEntity>) {
             .bottomBorder(1.dp, Color.Black.copy(alpha = 0.1f))
     ) {
         items(items = categories) { category ->
-            CategoryChips(title = category.category)
+            CategoryChips(
+                title = category.category,
+                isSelected = selectedChip == category.category,
+                onFilterCategory = { selectedChip = it }
+            )
         }
     }
 }
 
 @Composable
-fun CategoryChips(title: String) {
+private fun CategoryChips(title: String, isSelected: Boolean, onFilterCategory: (String) -> Unit) {
     Box(
         modifier = Modifier
             .padding(horizontal = 4.dp, vertical = 8.dp)
-            .clip(RoundedCornerShape(100.dp))
-            .background(MaterialTheme.colorScheme.primary)
-            .padding(horizontal = 12.dp, vertical = 8.dp)
+            .border(
+                width = 2.dp,
+                color = if (isSelected) Color.Transparent else MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .background(
+                shape = RoundedCornerShape(100.dp),
+                color = if (isSelected) MaterialTheme.colorScheme.primary else Color.Transparent,
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable { onFilterCategory(title) }
     ) {
         Text(
             title,
-            color = MaterialTheme.colorScheme.onPrimary,
+            color = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary,
             fontSize = 12.sp,
             fontWeight = FontWeight.SemiBold,
         )
