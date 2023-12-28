1c7ec1d4922323eb63180339ae2332ba85d5cd95
zuludin <zmauludin04@gmail.com>
2023-12-28 11:07:57 +0700

    change component dialog

--- app/src/main/java/com/app/zuludin/bookber/util/components/ConfirmAlertDialog.kt
+++ app/src/main/java/com/app/zuludin/bookber/util/components/ConfirmAlertDialog.kt
@@ -1,56 +1,39 @@
 package com.app.zuludin.bookber.util.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConfirmAlertDialog(message: String, onDismissRequest: () -> Unit, onConfirm: () -> Unit) {
     AlertDialog(
+        modifier = Modifier.shadow(0.dp, ambientColor = Color.Red),
         onDismissRequest = onDismissRequest,
-        content = {
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.White)
-                    .padding(16.dp)
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = message,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Row {
-                        TextButton(onClick = {
-                            onConfirm()
-                            onDismissRequest()
-                        }) {
-                            Text(text = "Yes")
-                        }
-                        TextButton(onClick = onDismissRequest) {
-                            Text(text = "No")
-                        }
-                    }
-                }
+        confirmButton = {
+            TextButton(onClick = {
+                onConfirm()
+                onDismissRequest()
+            }) {
+                Text(text = "Yes")
             }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = "No")
+            }
+        },
+        title = {
+            Text(
+                text = message,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
         },
     )
 }
